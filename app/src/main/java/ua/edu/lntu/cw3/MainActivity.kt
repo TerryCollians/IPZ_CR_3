ce0f65446541ecdc9db3eaf7edd10d31f7418d19
Владислав Павлик <144818450+TerryCollians@users.noreply.github.com>
2024-02-24 11:35:36 +0100

    added list

--- app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ua.edu.lntu.cw3.ui.theme.IPZ_CR_3Theme
+import androidx.compose.foundation.ScrollState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +32,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun VerrticalSpisok() {          // Функція вертикального списку
-
+    val items = listOf(           // Змінна зі значенням списку
+        Triple("Сніданок", "Омлет та смажені сосиски з соусом", "8:00"),
+        Triple("Обід", "Борщ з галушками та сметаною", "13:00"),
+        Triple("Вечеря", "Баранина в вершковому соусі з гарніром", "17:00")
+        // 3 довільних елемента
+    )
 }
 
 @Preview(showBackground = true)     // Прев'ю для списку
